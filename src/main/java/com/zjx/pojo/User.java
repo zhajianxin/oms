8d7c9d1cee5de6a82821cb4d81e491a8af25f902
sinoservices <sinoservices-sh@outlook.com>
2020-06-28 11:57:10 +0800

    user message commit

--- src/main/java/com/zjx/pojo/User.java
+++ src/main/java/com/zjx/pojo/User.java
@@ -3,7 +3,37 @@ package com.zjx.pojo;
 public class User {
 
 	public User() {
-		// TODO Auto-generated constructor stub
+		 
+	}
+	
+	private int id;
+	
+	private String name;
+	
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 }
