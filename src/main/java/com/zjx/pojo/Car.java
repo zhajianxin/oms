26aa6f9def215f6e451590109e3c7d1bd2cfe2c7
sinoservices <sinoservices-sh@outlook.com>
2020-07-01 14:44:39 +0800

    tij

---
+++ src/main/java/com/zjx/pojo/Car.java
@@ -0,0 +1,16 @@
+package com.zjx.pojo;
+
+public class Car {
+
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private int id;
+	
+	private double price;
+	
+	private String color;
+	
+	
+}
